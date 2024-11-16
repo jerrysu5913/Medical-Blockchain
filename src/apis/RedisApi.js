7a0a80409fc9f2a80a8931305b75d9793a2e3659
jerrysu5379 <jerrysu@fds.moe>
2024-11-16 16:32:16 +0800

    update

--- src/apis/RedisApi.js
+++ src/apis/RedisApi.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const fetch = () => {
   return axios.create({
-    baseURL: 'http://localhost:8081',
+    baseURL: 'https://medical-blockchain.pmine.org/',
     headers: {
       'accept': 'application/json'
     }
